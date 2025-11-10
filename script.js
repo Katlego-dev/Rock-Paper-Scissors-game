ffa765d7530b4e92ec4ae5cad1f85bd05aba7832
Katlego <aphane095@gmail.com>
2025-11-10 20:40:27 +0200

    added script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//function that randomly returns “rock”, “paper” or “scissors”
+
 function getComputerChoice(){
     const options = ["Rock", "Paper", "Scissors"];
     return options[Math.floor(Math.random() * options.length)];
