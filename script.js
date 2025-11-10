b2ccc44da9f706839a9f9764438d4984ec3301c6
Katlego <aphane095@gmail.com>
2025-11-10 20:50:18 +0200

    created function to get input from the user and return it

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ function getComputerChoice(){
     const options = ["Rock", "Paper", "Scissors"];
     return options[Math.floor(Math.random() * options.length)];
 }
+
+//function to get the human choice
+
+function getHumanChoice(){
+    const choice = prompt("Choose between Rock, Paper, Scissors: ");
+    return choice;
+}
+
+
