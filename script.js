8c4c329ddfe929bdfe808db3a67092f1f2bfa465
Katlego <aphane095@gmail.com>
2025-11-15 15:44:26 +0200

    Added a feauture to increase the score after every round, and a message of the current score

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const humanScore = 0;
-const computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
 //function that randomly returns “rock”, “paper” or “scissors”
 
@@ -15,34 +15,50 @@ function getHumanChoice(){
     return choice.toLowerCase();
     
 }
+//function to compare user input and computer choice
  function playRound(humanChoice, computerChoice){
     
     if(humanChoice === "rock" && computerChoice === "rock"){
-        alert("Its a Draw, both chose Rock");
-       
+        alert("Its a tie, you both chose Rock");
+        alert("The score is: " + humanScore + " - " + computerScore);
+          
     }else if(humanChoice === "rock" && computerChoice === "paper"){
         alert("Computer wins, Paper covers Rock");
+        computerScore++;
+        alert("The score is: " + humanScore + " - " + computerScore);
 
     }else if(humanChoice === "rock" && computerChoice === "scissors"){
         alert("You Win, Rock crushes Scissors");
-
+         humanScore++;
+        alert("The score is: " + humanScore + " - " + computerScore);
+        
     }else if(humanChoice === "paper" && computerChoice === "scissors"){
         alert("Computer Wins, Scissors cut Paper");
+        computerScore++;
+        alert("The score is: " + humanScore + " - " + computerScore);
 
     }else if(humanChoice === "paper" && computerChoice === "rock"){
         alert("You Win, Paper covers Rock");
+        humanScore++;
+        alert("The score is: " + humanScore + " - " + computerScore);
 
     }else if(humanChoice === "paper" && computerChoice === "paper"){
-        alert("Its a Draw, you both chose Paper");
+        alert("Its a Tie, you both chose Paper");
+        alert("The score is: " + humanScore + " - " + computerScore);
 
     }else if(humanChoice === "scissors" && computerChoice === "rock"){
         alert("Computer Wins, Rock crushes Scissors");
+        computerScore++;
+        alert("The score is: " + humanScore + " - " + computerScore);
     
     }else if(humanChoice === "scissors" && computerChoice === "scissors"){
-        alert("Its a Draw, you both chose Scissors");
+        alert("Its a Tie, you both chose Scissors");
+        alert("The score is: " + humanScore + " - " + computerScore);
 
     }else if(humanChoice === "scissors" && computerChoice === "paper"){
         alert("You Win, Scissors cut Paper");
+        humanScore++;
+        alert("The score is: " + humanScore + " - " + computerScore);
     }
  }
  playRound(getHumanChoice(), getComputerChoice());
